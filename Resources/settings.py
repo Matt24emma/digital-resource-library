bb1904f680e0d9f114266309fc7d43a7c3ad4f51
Matthew Emmanuel <libertyvictor356@gmail.com>
2026-07-21 19:44:20 +0100

    Secure secret key configuration

--- Resources/settings.py
+++ Resources/settings.py
@@ -82,10 +82,14 @@ WSGI_APPLICATION = 'Resources.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        default=os.environ.get("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
         conn_max_age=600,
     )
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.onrender.com",
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
