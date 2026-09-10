bd6ef59486f36d5fbbb7bd22f741b0949813fbc2
Matthew Emmanuel <libertyvictor356@gmail.com>
2026-09-11 00:18:20 +0100

    Auto-create superuser on deploy via env vars

--- build.sh
+++ build.sh
@@ -5,3 +5,20 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
+
+
+# Create superuser if it doesn't exist
+python manage.py shell << 'EOF'
+from django.contrib.auth.models import User
+import os
+
+username = os.environ.get("SUPERUSER_USERNAME")
+email = os.environ.get("SUPERUSER_EMAIL")
+password = os.environ.get("SUPERUSER_PASSWORD")
+
+if username and password and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f"Superuser {username} created.")
+else:
+    print("Superuser exists or env vars not set. Skipping.")
+EOF
